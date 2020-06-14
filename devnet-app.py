f2a8edf1f7de5b7f940201952a6a4afccd1f69d3
Kelvin <kelvincbrooksjr@gmail.com>
2020-06-14 15:39:36 -0500

    Fixed css links and resources title

--- devnet-app.py
+++ devnet-app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html", title = "Home Page")
+    return render_template("index.html", title = "Home")
 
 @app.route("/content")
 def content():
